7b0d7052cb541e5b5e67ca24ceea0a799de9a959
BiriGica <gigipoligarcia@gmail.com>
2024-12-12 10:22:44 -0300

    Update models.py

--- artemis/Artemis_app/models.py
+++ artemis/Artemis_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Usuário(models.Model):
+    relato =  models.CharField (max_length=1500)
+
+
+    #método da classe
+    def registrar_relato (self):
+        self.save()
